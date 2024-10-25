cf8080aa6a4796084b83d3e4b02e5b8d1ad36f4f
Reydeuss <winsonamadeus.kusuma@gmail.com>
2024-10-25 15:52:13 +0700

    Ideafuse CPP solution

---
+++ tlx/other/ideafuse/ideafuse.cpp
@@ -0,0 +1,13 @@
+// Code by Reydeuss @ Github
+// Original Problem Name: IdeaFuse
+// Problem Link: https://tlx.toki.id/problems/ideafuse-2014-icpc-sumatra-qual/B
+// Submission Link: https://tlx.toki.id/problems/ideafuse-2014-icpc-sumatra-qual/B/submissions/2794776
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "IdeaFuse";
+    return 0;
+}
