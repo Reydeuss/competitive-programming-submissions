26b7380cbe7fff19c1d3e8a86f8be7d26dced05f
Reydeuss <winsonamadeus.kusuma@gmail.com>
2024-10-25 17:04:49 +0700

    TLX Modulo Problem Solution in Java

---
+++ tlx/math/modulo/Modulo.java
@@ -0,0 +1,15 @@
+// Code & Solution by Reyydeuss @ Github
+// Problem Name: Modulo
+// Objective: Given an integer N, find the number that can give the largest modulo to N
+// Problem Link: https://tlx.toki.id/problems/troc-26/A/
+// Submission: https://tlx.toki.id/problems/troc-26/A/submissions/2795287
+
+import java.util.Scanner;
+
+public class Modulo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(n + 1);
+    }
+}
