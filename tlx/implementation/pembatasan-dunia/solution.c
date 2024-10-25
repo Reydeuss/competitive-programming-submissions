671f3e7f2977b50c9ae767133c091a09063e19f7
Reydeuss <winsonamadeus.kusuma@gmail.com>
2024-10-25 16:00:09 +0700

    Pembatasan Dunia C solution

---
+++ tlx/implementation/pembatasan-dunia/solution.c
@@ -0,0 +1,12 @@
+// Code by Reyydeus @ Github
+// Submission: https://tlx.toki.id/problems/troc-12/A/submissions/2794906
+
+#include <stdio.h>
+
+int main() {
+    int a, x;
+    scanf("%d %d", &a, &x);
+    printf("%d %d", x/a, x%a);
+
+    return 0;
+}
