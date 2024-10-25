b2c45c2c1c9f65969f180de9daf7f1f1799ccb17
Reydeuss <winsonamadeus.kusuma@gmail.com>
2024-10-25 16:07:47 +0700

    TLX Modulo C solution

---
+++ tlx/math/modulo/solution.c
@@ -0,0 +1,15 @@
+// Code & Solution by Reyydeuss @ Github
+// Problem Name: Modulo
+// Objective: Given an integer N, find the number that can give the largest modulo to N
+// Problem Link: https://tlx.toki.id/problems/troc-26/A/
+// Submission: https://tlx.toki.id/problems/troc-26/A/submissions/2794996
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d", n+1);
+
+    return 0;
+}
