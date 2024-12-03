c82ba5f27576c4117a492db64f1f7851ac4c8893
Reydeuss <winsonamadeus.kusuma@gmail.com>
2024-12-03 21:11:18 +0700

    TLX Modul - C++ solution

---
+++ tlx/math/modulo/modulo.cpp
@@ -0,0 +1,15 @@
+// Code & Solution by Reyydeuss @ Github
+// Problem Name: Modulo
+// Objective: Given an integer N, find the number that can give the largest modulo to N
+// Problem Link: https://tlx.toki.id/problems/troc-26/A/
+
+#include <iostream>
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    std::cout << n + 1 << std::endl;
+
+    return 0;
+}
