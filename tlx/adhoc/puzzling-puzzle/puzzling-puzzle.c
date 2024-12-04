feeb795ee3e5b163bcec5f9a02e445b353a59b40
Reydeuss <winsonamadeus.kusuma@gmail.com>
2024-12-04 19:04:37 +0700

    tlx puzzling puzzle c solution

---
+++ tlx/adhoc/puzzling-puzzle/puzzling-puzzle.c
@@ -0,0 +1,13 @@
+// Code & Solution by Reydeuss @ Github
+// Original Problem Title: Puzzling Puzzle
+// Original Problem Link: https://tlx.toki.id/problems/troc-16/A
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("1 %d\n", n);
+
+    return 0;
+}
