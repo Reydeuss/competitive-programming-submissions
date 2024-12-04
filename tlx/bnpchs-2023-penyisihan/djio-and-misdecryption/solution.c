ff6715a53cdfcae7ad5c562922ce978cb2f0c6c7
Reydeuss <winsonamadeus.kusuma@gmail.com>
2024-12-04 19:49:05 +0700

    BNPCHS 2023 - Penyisihan Problem A solution in C

---
+++ tlx/bnpchs-2023-penyisihan/djio-and-misdecryption/solution.c
@@ -0,0 +1,31 @@
+// Code & Solution by Reydeuss @ Github
+// Original Problem Title: Djio and Misdecryption
+// Original Problem Link: https://tlx.toki.id/problems/bnpchs-2023-penyisihan/A
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main() {
+    int n;
+    int i = 0;
+    char c;
+    bool safe = true;
+    
+    scanf("%d", &n); getchar();
+
+    while (i++ < n) {
+        c = getchar();
+        if (c == '*') {
+            safe = !safe;
+            continue;
+        }
+
+        if (safe) {
+            putchar(c);
+        }
+    }
+    putchar('\n');
+
+    return 0;
+}
