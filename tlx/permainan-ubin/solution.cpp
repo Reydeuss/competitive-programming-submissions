4e9a3f29d90a351538ecbbcd29356bdc62b6951d
Raynaldo Winson <winsonamadeus.kusuma@gmail.com>
2025-09-18 15:08:15 +0700

    added cpp solution to TROC #14 - A. Permainan Ubin (TLX)

---
+++ tlx/permainan-ubin/solution.cpp
@@ -0,0 +1,20 @@
+/**
+  * Submission & solution by Reydeus
+  * Title: TROC #14 - A. Permainan Ubin
+  * Original Problem: https://tlx.toki.id/problems/troc-14/A
+  * Submission: https://tlx.toki.id/problems/troc-14/A/submissions/4332088
+*/
+
+#include <bits/stdc++.h>
+
+int main()
+{
+    int n, m;
+
+    std::cin >> n;
+    std::cin >> m;
+
+    std::cout << n*m;
+
+    return 0;
+}
