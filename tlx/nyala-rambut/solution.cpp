ed820d371b4d72b42d849bae592a67c229d2c2e1
Raynaldo Winson <winsonamadeus.kusuma@gmail.com>
2025-09-23 12:30:48 +0700

    added TROC #21 A - Nyala Rambut solution in C++ (TLX)

---
+++ tlx/nyala-rambut/solution.cpp
@@ -0,0 +1,21 @@
+// Submission & solution by Reydeus
+// Title: TROC #21 A - Nyala Rambut
+// Original Problem: https://tlx.toki.id/problems/troc-21/A
+// Submission: https://tlx.toki.id/problems/troc-21/A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n, d;
+
+    cin >> n >> d;
+    if (d > 1) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
